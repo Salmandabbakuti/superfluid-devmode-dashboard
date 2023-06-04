5b4ac5a5a2c84f238a728101e6a6bbfd0d4b9570
Salman Dabbakuti <dabbakutisalman@gmail.com>
2023-06-04 18:47:36 +0530

    removed: unnecessary files and redundant functionalities

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,8 @@
 require('@nomicfoundation/hardhat-toolbox');
 
-task("deploy", "Deploys Contract", async () => {
-  const contractFactory = await ethers.getContractFactory("Greeter");
-  const contract = await contractFactory.deploy("Hello, Hardhat!");
-  await contract.deployed();
-  console.log("contract deployed at:", contract.address);
-});
-
-
 module.exports = {
-  solidity: "0.8.16",
   defaultNetwork: "local",
   networks: {
-    hardhat: {
-      chainId: 31337
-    },
     local: {
       url: "http://127.0.0.1:8545",
     }
